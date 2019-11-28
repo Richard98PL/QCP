49c1dee7dbccce8ce1c4faf55e7494a1ae4d0456
Richard98PL <41301282+Richard98PL@users.noreply.github.com>
2019-11-28 16:17:37 +0100

    Update discountsOnServicesWithAmericaCondition.ts

--- discountsOnServicesWithAmericaCondition.ts
+++ discountsOnServicesWithAmericaCondition.ts
@@ -1,59 +1,52 @@
 export function onAfterCalculate(quote, lines) {
 
-   setDiscountOnServices(
-                        lines,
-                        calculateServicesQuantity(quote,lines)
-                        );
+  setDiscountOnServices(
+                       lines,
+                       calculateServicesQuantity(quote,lines)
+                       );
 
-  return Promise.resolve();
+ return Promise.resolve();
 };
 
 function calculateServicesQuantity(quote,lines){
-    let servicesQuantity = 0;
-    let doubleBundle3LinesMap = new Map(); // key - name
-                                           // value - quantity
-                                           // map of lines in the america condition bundle
+   let servicesQuantity = 0;
+   let doubleBundle3LinesSet = new Set();
 
-    lines.forEach(function(line){
-        let lineParent = line.parentItem;
-        if(line.record["SBQQ__Product__r"]["Family"] == "Service"){
-            if(lineParent.record["SBQQ__ProductName__c"] == "Double Bundle 3 (other than America criteria)"){
-              doubleBundle3LinesMap.set(
-                        /*key*/         line.record["SBQQ__ProductName__c"], 
-                        /*value*/       line.record["SBQQ__Quantity__c"]
-                                        );
-        }else{
-            servicesQuantity += line.record["SBQQ__Quantity__c"];
-        }
-    }
-      });
+   lines.forEach(function(line){
+       let lineParent = line.parentItem;
+       if(line.record["SBQQ__Product__r"]["Family"] == "Service"){
+           if(lineParent.record["SBQQ__ProductName__c"] == "Double Bundle 3 (other than America criteria)"){
+             doubleBundle3LinesSet.add(line.record["SBQQ__ProductName__c"]);
+       }else{
+           servicesQuantity++;
+       }
+   }
+     });
 
-    let isOnlyAmericaSelected = doubleBundle3LinesMap.size == 1 && 
-                                doubleBundle3LinesMap.has("America");
+   let isOnlyAmericaSelected = doubleBundle3LinesSet.size == 1 && 
+                               doubleBundle3LinesSet.has("America");
 
-    if(!isOnlyAmericaSelected){
-      for (let [k, v] of doubleBundle3LinesMap) {
-        servicesQuantity += v;
-      }
-    }
+   if(!isOnlyAmericaSelected){
+     servicesQuantity += doubleBundle3LinesSet.size;
+   }
 
-    return servicesQuantity;
+   return servicesQuantity;
 }
 
 function setDiscountOnServices(lines,servicesQuantity){
-  let discount = 0;
+ let discount = 0;
 
-  if(servicesQuantity > 3){
-    discount = 0.09;
-  }else if(servicesQuantity > 5){
-    discount = 0.13;
-  }else if(servicesQuantity > 10){
-    discount = 0.2;
-  }
-  
-  lines.forEach(function(line){
-    if(line.record["SBQQ__Product__r"]["Family"] == "Service"){
-      line.record["SBQQ__NetPrice__c"] -= discount * line.record["SBQQ__NetPrice__c"];
-    }    
-  });
+ switch(servicesQuantity){
+   case 1: discount = 0.13; break;
+   case 2: discount = 0.15; break;
+   case 3: discount = 0.17; break;
+   case 4: discount = 0.19; break;
+   case 5: discount = 0.2; break;
+ }
+ 
+ lines.forEach(function(line){
+   if(line.record["SBQQ__Product__r"]["Family"] == "Service"){
+     line.record["SBQQ__NetPrice__c"] -= discount * line.record["SBQQ__NetPrice__c"];
+   }    
+ });
 }
