97e2cc768ce06f12a58fb4bd857dad178251288f
Richard98PL <41301282+Richard98PL@users.noreply.github.com>
2019-12-18 12:47:43 +0100

    Update bundleDiscountsBasedOnLines.ts

--- bundleDiscountsBasedOnLines.ts
+++ bundleDiscountsBasedOnLines.ts
@@ -15,7 +15,7 @@ export function onAfterCalculate(quote, lines) {
         let isChildrenOfDiscountableBundle = parentsOfG3CWithChildrenQuantities.has(lineParent);
 
         if(isChildrenOfDiscountableBundle){
-            let discount = getAppropriateDiscount(parentsOfG3CWithChildrenQuantities.get(lineParent));
+            let discount = getTierDiscount(parentsOfG3CWithChildrenQuantities.get(lineParent));
             line.record["SBQQ__NetPrice__c"] *= 1-discount; 
         }
    });
@@ -47,57 +47,50 @@ export function onAfterCalculate(quote, lines) {
      return parentsOfG3CWithItsChildrenQuantities;
   }
 
-  function getAppropriateDiscount(quantityOfG3CLines){
-      let tier = getTier(quantityOfG3CLines);
-      switch(tier){
-          case "Tier 1": return 0.15;
-          case "Tier 2": return 0.40;
-          case "Tier 3": return 0.50;
-          case "Tier 4": return 0.60;
-          case "Tier 5": return 0.70;
-          case "Tier 6": return 0.75;
-          case "Tier 7": return 0.78;
-          default: return 0;
-      }
-  }
-
-  function getTier(quantityOfG3CLines){
+  function getTierDiscount(quantityOfG3CLines){
       
     const tier = {
 
         tiersArray : new Array(
           {TierLowerBound : 2,
           TierUpperBound : 5,
-          TierName : 'Tier 1'},
+          TierName : 'Tier 1',
+          TierDiscount : 0.15},
 
           {TierLowerBound : 6,
           TierUpperBound : 10,
-          TierName : 'Tier 2'},
+          TierName : 'Tier 2',
+          TierDiscount : 0.40},
 
           {TierLowerBound : 11,
           TierUpperBound : 15,
-          TierName : 'Tier 3'},
+          TierName : 'Tier 3',
+          TierDiscount : 0.50},
 
           {TierLowerBound : 16,
           TierUpperBound : 20,
-          TierName : 'Tier 4'},
+          TierName : 'Tier 4',
+          TierDiscount : 0.60},
 
           {TierLowerBound : 21,
           TierUpperBound : 30,
-          TierName : 'Tier 5'},
+          TierName : 'Tier 5',
+          TierDiscount : 0.70},
 
           {TierLowerBound : 31,
           TierUpperBound : 40,
-          TierName : 'Tier 6'},
+          TierName : 'Tier 6',
+          TierDiscount : 0.75},
 
           {TierLowerBound : 41,
           TierUpperBound : 55,
-          TierName : 'Tier 7'},
+          TierName : 'Tier 7',
+          TierDiscount : 0.78},
         ),       
         checkWhichTier : function(number){
           for(let i = 0 ; i < tier.tiersArray.length ; i++){
            if(tier.isBetween(number, tier.tiersArray[i].TierLowerBound, tier.tiersArray[i].TierUpperBound)){
-              return tier.tiersArray[i].TierName;
+              return tier.tiersArray[i].TierDiscount;
            }
           }    
         },     
@@ -108,7 +101,7 @@ export function onAfterCalculate(quote, lines) {
     }
   }
  
-    return tier.checkWhichTier(quantityOfG3CLines);
+    return tier.checkWhichTier(quantityOfG3CLines) || 0;
   }
 
 
